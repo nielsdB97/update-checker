3add490ce18bbb4cd17e8395d797d4c52257dc6e
Niels de Bruin <nielsdb97@gmail.com>
2020-05-22 16:29:28 +0200

    Send Telegram message if update is available

--- updateChecker.go
+++ updateChecker.go
@@ -6,13 +6,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	godotenv "github.com/joho/godotenv"
 )
 
 const ls50FileName = "ls50_release_notes"
 const ls50url = "https://assets.kef.com/pdf_doc/ls50w/LS50-Wireless-Firmware-Release-Note.pdf"
 
 func main() {
+	dotenvErr := godotenv.Load()
+	if dotenvErr != nil {
+		panic(dotenvErr)
+	}
 	fileContent, readErr := ioutil.ReadFile("/tmp/" + ls50FileName)
 
 	if readErr != nil {
@@ -51,4 +60,20 @@ func main() {
 	if writeErr != nil {
 		panic(writeErr)
 	}
+
+	bot, botErr := tgbotapi.NewBotAPI(os.Getenv("TG_API_TOKEN"))
+	if botErr != nil {
+		panic(botErr)
+	}
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	chatID, parseErr := strconv.ParseInt(os.Getenv("TG_CHAT_ID"), 10, 64)
+	if parseErr != nil {
+		panic(parseErr)
+	}
+	msg := tgbotapi.NewMessage(chatID, "An update is available for the KEF LS50 Wireless!")
+
+	bot.Send(msg)
 }
