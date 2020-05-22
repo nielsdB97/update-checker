3de75b7ea01262e0ed0c1ab8b803bcd6558b123d
Niels de Bruin <nielsdb97@gmail.com>
2020-05-22 14:33:14 +0200

    Read previous file and check if it's equal

--- updateChecker.go
+++ updateChecker.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const ls50FileName = "ls50_release_notes"
 const ls50url = "https://assets.kef.com/pdf_doc/ls50w/LS50-Wireless-Firmware-Release-Note.pdf"
 
 func main() {
+	fileContent, readErr := ioutil.ReadFile("/tmp/" + ls50FileName)
+
+	if readErr != nil {
+		if !strings.Contains(readErr.Error(), "no such file or directory") {
+			panic(readErr)
+		}
+	}
+
 	resp, httpErr := http.Get(ls50url)
 
 	if httpErr != nil {
@@ -19,6 +30,11 @@ func main() {
 
 	if readErr != nil {
 		panic(readErr)
+	}
+
+	if bytes.Equal(fileContent, body) {
+		fmt.Println("Files match!")
+		return
 	}
 
 	writeErr := ioutil.WriteFile("/tmp/"+ls50FileName, body, 0644)
